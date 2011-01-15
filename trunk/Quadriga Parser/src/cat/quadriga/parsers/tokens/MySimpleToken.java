f291041982f45290ff1559592aeb026884f60f52
atridas87 <atridas87@81ec1f47-8d4a-a2d7-6a82-9d82ac2d197c>
2011-01-15 15:22:12 +0000

    Començat el primer parser.

---
+++ trunk/Quadriga Parser/src/cat/quadriga/parsers/tokens/MySimpleToken.java
@@ -0,0 +1,32 @@
+package cat.quadriga.parsers.tokens;
+
+import cat.quadriga.parsers.Token;
+import cat.quadriga.parsers.QuadrigaSimple;
+
+public class MySimpleToken extends Token
+{
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * Constructs a new token for the specified Image and Kind.
+   */
+  public MySimpleToken(int kind, String image)
+  {
+     this.kind = kind;
+     this.image = image;
+  }
+
+  public int realKind = QuadrigaSimple.GT;
+
+  /**
+   * Returns a new Token object.
+  */
+
+  public static final Token newToken(int ofKind, String tokenImage)
+  {
+    return new MySimpleToken(ofKind, tokenImage);
+  }
+}
